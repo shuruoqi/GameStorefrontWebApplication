fbcdc26b5e22e0107ef0f054e664c456ae0a9b26
shuruoqi <shuruoqiapp@163.com>
2020-11-14 11:41:36 +0800

    Change IMG folder name

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
     {
         $dbhost = "localhost";
         $dbuser = "root";
-        $dbpass = "";
+        $dbpass = "root";
         $db = "project";
         $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
         return $conn;
