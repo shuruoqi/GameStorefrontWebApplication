31dbf0a8d7a56169b9a3c8377a057d615a52e934
ChristyQu <yangqu.0705@gmail.com>
2020-11-14 22:49:02 -0800

    create sign in page-finished

--- SignIn.php
+++ SignIn.php
@@ -1,47 +1,52 @@
 <?php
 include 'connect.php';
-// var_dump($_POST);
 
-header("Location:Player-Bank.php");
+if (isset($_POST["UserID"]) and isset($_POST["Password"])) {
 
-$id = 0;
-for ($i = 1; $i <= 2; $i++) {
-    $n = floor(rand(1, 500) * 20 + 10);
-    $id = $id + $n;
-}
-// echo "Your id is: ". $id. "  ";
+    $userID = $_POST['UserID'];
+    $password = $_POST['Password'];
 
-if (!empty($_POST["username"]) and !empty($_POST["password"]) and !empty($_POST["location"])) {
+    session_start();
+    $_SESSION['currentID']= $userID;
 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $location = $_POST['location'];
-    $type = $_POST['type'];
+    if(!empty($_POST['type'])){
+        $radio = $_POST['type'];
+    }
 
     $conn = OpenCon();
-    if ($type === 'producer') {
-        $sql = "INSERT INTO producer (companyID, companyName, companyPW, location,totalProduced) VALUES ('$id', '$username','$password','$location','0')";
-    } else {
-        $sql = "INSERT INTO player (playerID, username, playerPW, location) VALUES ('$id', '$username', '$password', '$location');";
-        $sql .= "INSERT INTO RegularMember (playerID) VALUES ('$id');";
+    switch($radio){
+        case Producer:
+        $sql = "SELECT * FROM Producer WHERE companyID = '$userID' and companyPW ='$password'";
+        $result = mysqli_query($conn,$sql) or die("Failed to query database ".mysql_error());
+    	  $row = mysqli_fetch_array($result);
+        if($row['companyID']===$userID && $row['companyPW'] ===$password){
+    		header('location:Player-Bank.php');
+    	}else{
+    		echo "Failed to login!";
+    	}
+        break;
+        case Player:
+        $sql = "SELECT * FROM Player WHERE playerID = '$userID' and playerPW ='$password'";
+        $result = mysqli_query($conn,$sql) or die("Failed to query database ".mysql_error());
+    	  $row = mysqli_fetch_array($result);
+        if($row['playerID']===$userID && $row['playerPW'] ===$password){
+    		header('location:Player-Bank.php');
+    	}else{
+    		echo "Failed to login!";
+    	}
+        break;
     }
-    if ($conn->multi_query($sql) === TRUE) {
-        echo "New user added---Your id is: " . $id;
-    } else {
-        echo "Error : " . $conn->error;
-    }
-} else {
-    echo "POST array is null";
 }
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="CSS/Center.css"/>
-    <link rel="stylesheet" href="CSS/SignUp.css"/>
-    <title>SignUp</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../CSS/Center.css"/>
+    <link rel="stylesheet" href="../CSS/iconfont-SignIn.css"/>
+    <link rel="stylesheet" href="../CSS/SignIn.css"/>
+    <title>SignIn</title>
 </head>
 
 <body>
@@ -49,55 +54,38 @@ if (!empty($_POST["username"]) and !empty($_POST["password"]) and !empty($_POST[
 <div id="Centered">
 
     <div class="MainBox">
+        <h1>SIGN IN</h1>
 
-        <h1>New User Sign up</h1>
-        <form method="POST"> <!--refresh page when submitted-->
-            <input type="hidden" id="insertQueryRequest" name="insertQueryRequest">
-            <!-- Your ID will be <h2 id="randomnumber"></h2> -->
-            <div class="inputBox">
-                <div class="inputText">
-                    User Name: <input type="text" name="username"> <br/><br/>
-                    Password: <input type="password" name="password"> <br/><br/>
-                    Confirm Password: <input type="password" name="password"> <br/><br/>
-                </div>
-                Location:
-                <select name="location">
-                    <optgroup label="location">
-                        <option value=Asia>Asia</option>
-                        <option value=Africa>Africa</option>
-                        <option value=NorthAmerica>North America</option>
-                        <option value=SouthAmerica>South America</option>
-                        <option value=Antarctica>Antarctica</option>
-                        <option value=Europe>Europe</option>
-                        <option value=Australia>Australia</option>
-                    </optgroup>
-                </select> <br/><br/>
-                <!--                    Device: <input type="text" name="password"> * <br/><br/>-->
-            </div>
+        <div checkBox>
+          <form action="SignIn.php" method="POST"> <!--refresh page when submitted-->
+              <!-- <input type="hidden" name="ID" value="312"> -->
+              <input type="hidden" id="insertQueryRequest" name="insertQueryRequest">
 
             <div class="checkBox">
-                Select a account type:<br/><br/>
-                <input type="radio" id="player" name="type" value="player">
-                <label for="regularmember">Player</label> * <br/><br/>
-                <input type="radio" id="producer" name="type" value="producer">
-                <label for="producer">Producer</label>
+                <input type="radio" id="Player" name="type" value="Player" checked>
+                <label for="Player">Player</label>
+                <input type="radio" id="Producer" name="type" value="Producer">
+                <label for="Producer">Producer</label>
             </div>
-            <a href="Player-Bank.php">
-                <input class="SignUpButton" type="submit" value="Sign up"/>
-            </a>
+        </div>
 
+        <div class="inputBox">
+            <div class="inputText">
+                <span class="iconfont icon-nickname"></span>
+                <input type="text" name="UserID" placeholder="UserID"/> <br/><br/>
+                <span class="iconfont icon-visible"></span>
+                <input type="password" name="Password" placeholder="Password"/>
+            </div>
+        </div>
+
+        <input class="loginButton" type="submit" value="Login">
+
+        <p>
+            Don't have an account? <a href="SignUp.php">Join us!</a>
+        </p>
         </form>
     </div>
 </div>
 </body>
-
-<!-- <script>
-    var rndnumb = "";
-    for (i = 1; i <= 2; i++) {
-        n = Math.floor(Math.random() * 89999 + 10000);
-        rndnumb = rndnumb + n;
-    }
-    document.getElementById("randomnumber").innerHTML = rndnumb;
-</script> -->
 
 </html>
